4cb025557795c2b21875d00be5e462dd6b3fd2d8
James Carroll <jc2pointzero@gmail.com>
2026-02-26 01:54:15 -0500

    chore: move firebase config to environment variables

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDcRYiCrrpyzUFIESePGTD2b1SKzl2MPE",
-  authDomain: "greenstack-13c6a.firebaseapp.com",
-  projectId: "greenstack-13c6a",
-  storageBucket: "greenstack-13c6a.firebasestorage.app",
-  messagingSenderId: "701791787547",
-  appId: "1:701791787547:web:9d1cc2ccb0bb364bf9f6be",
-  measurementId: "G-3FW33NL6DX"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
